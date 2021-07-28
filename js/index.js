6d2da35d112fe5b19091be1c64aba71767ef9b35
NourAdawi <nouradwi12@gmail.com>
2021-07-28 10:51:07 +0300

    get name of the current location Relates #15

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 // Dom.js
 const searchQuery = document.querySelector(".value_search");
 const searchBtn = document.querySelector(".submit_search");
+const frame = document.querySelector(".iframe");
+const cityName= document.querySelector(".info_area-state");
 
-// map location user firest time
-let map = document.querySelector('.map')
-window.addEventListener('load',()=>{
-    navigator.geolocation.getCurrentPosition(xy=>{
-        let  {latitude, longitude} = xy.coords
-        sendMap(latitude, longitude)
+
+// defult location
+const getStartedLocation = (() => {
+  navigator.geolocation.getCurrentPosition((xy) => {
+    let { latitude, longitude } = xy.coords;
+    let url= `https://maps.google.com/maps?q=${latitude},${longitude}&hl=es&z=14&amp&output=embed`;
+    sendMap(latitude, longitude);
+    getWeather(latitude, longitude);
+    getRequest(url, (data)=> {
+      cityName.textContent= data[0].display_name;
 
     })
-})
-
+  });
+})();
 
 // logic.js
 
@@ -32,27 +38,17 @@ searchBtn.addEventListener("click", () => {
   let query = searchQuery.value;
   let url = `https://nominatim.openstreetmap.org/search.php?q=${query}&format=jsonv2`;
   getRequest(url, (data) => {
+    console.log(data);
     let lat= data[0].lat;
     let lon= data[0].lon;
     sendMap(lat, lon)
-    console.log(lat, lon);
     
   });
 });
 
 
-
-
 // to select x,y and send
 function sendMap(latitude, longitude){
-    removeChild(map)
-    map.innerHTML = `<iframe class="iframe" src="https://maps.google.com/maps?q=${latitude},${longitude}&hl=es&z=14&amp;output=embed"></iframe>`
-}
-
-// for remove all content main
-function removeChild(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+    frame.src = `https://maps.google.com/maps?q=${latitude},${longitude}&hl=es&z=14&amp&output=embed`;
 }
 
